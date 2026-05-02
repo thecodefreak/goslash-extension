c166536117c1d8644602f25fef8b6daeb56dd307
thecodefreak <amaljohnronkha@protonmail.com>
2026-05-02 12:57:30 +0100

    ui tweaks

--- options/state.js
+++ options/state.js
@@ -44,8 +44,8 @@ export const TOAST_ICONS = {
 };
 
 export const ACTION_ICONS = {
-  edit: `<svg class="ui-icon" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><path d="M4.75 14.5v.75h.75l7.97-7.97-.75-.75L4.75 14.5Zm9.53-9.53.75.75.47-.47a1.06 1.06 0 0 0-1.5-1.5l-.47.47Z" fill="currentColor"/></svg>`,
-  delete: `<svg class="ui-icon" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><path d="M6.25 6.25h7.5M8 6.25V5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v1.25m-4.25 0-.5 8a1 1 0 0 0 1 .99h3.5a1 1 0 0 0 1-.99l-.5-8" fill="none" stroke="currentColor" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"/></svg>`
+  edit: `<svg class="ui-icon" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" focusable="false"><path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.919 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z"/><path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z"/></svg>`,
+  delete: `<svg class="ui-icon" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" focusable="false"><path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482 41.03 41.03 0 0 0-2.595-.34V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z" clip-rule="evenodd"/></svg>`
 };
 
 export function clearGroupEditState(options = {}) {
